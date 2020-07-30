f079fc42f0c5f036f61d3e7f68b5ec7888f91d4c
泥萌的法师 <fubaimaomei.com>
2020-07-30 17:29:17 +0800

    add schema.js

---
+++ start/server/src/schema.js
@@ -0,0 +1,56 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  # 描述数据图中的对象
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
+
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+
+  # 定义数据图所要执行的查询
+  # ID 为标量类型
+  type Query {
+    launches: [Launch]!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  type Mutation {
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+    cancleTrips(launchId: ID!): TripUpdateResponse!
+    login(email: String): String # login token
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch] # 返回修改后的任何对象，以便客户端及时跟新缓存和UI
+  }
+`;
+
+module.exports = typeDefs;
